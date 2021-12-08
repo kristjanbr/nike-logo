da7dbfa8bf96ab98d4558f2344c0e53a6a9ffeeb
kristjTSC <kristjanbratasevec1@gmail.com>
2021-12-08 21:36:26 +0100

    Minor changes 4

--- js/main.js
+++ js/main.js
@@ -8,15 +8,20 @@ window.onload = function() {
     var htmlCanvas = document.getElementById('canvas'),
     ctx = htmlCanvas.getContext('2d');
 
-    
-    ctx.scale(0.3, 0.3);
     ctx.beginPath();
 
-    ctx.moveTo(280, 390);
-    ctx.bezierCurveTo(582, 500, 1134, 212, 1820, 20);
-    ctx.bezierCurveTo(624, 539, 320, 720, 130, 630);
-    ctx.bezierCurveTo(-134, 460, 214, 130, 312, 10);
-    ctx.bezierCurveTo(160, 212, 231, 363, 334, 413);
+    ctx.moveTo(55, 1);
+    ctx.bezierCurveTo(35, 20, 16, 46, 7, 66);
+    ctx.bezierCurveTo(-8, 104, 1, 136, 32, 141);
+    ctx.bezierCurveTo(55, 145, 85, 137, 131, 117);
+    ctx.bezierCurveTo(137, 114, 179, 96, 304, 42);
+    ctx.bezierCurveTo(357, 20, 390, 2, 399, 2);
+    ctx.bezierCurveTo(399, 2, 116, 77, 108, 79);
+    ctx.bezierCurveTo(95, 83, 80, 83, 69, 81);
+    ctx.bezierCurveTo(51, 77, 42, 63, 42, 42);
+    ctx.bezierCurveTo(42, 34, 46, 21, 54, 5);
+    ctx.bezierCurveTo(55, 2, 57, 0.1, 57, 0.05);
+    ctx.bezierCurveTo(57, 0, 56, 1, 55, 2);
 
     ctx.closePath();
     ctx.fillStyle="#191919";
@@ -28,7 +33,7 @@ function about(){
     Swal.fire({
         position: 'top',
         title: 'Credits',
-        text: 'Made by Kristjan Brataševec, 4.RA, 2021',
+        text: 'Made by Kristjan Brataševec, 4. RA, 2021',
         icon: 'info',
         showConfirmButton: false,
     })
